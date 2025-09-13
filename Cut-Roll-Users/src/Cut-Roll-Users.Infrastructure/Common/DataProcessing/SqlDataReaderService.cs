8bf68cd6b380aa57537491a856ebcdb85720299e
maksimkins <maks.aleshkov.04@gmail.com>
2025-09-13 21:24:01 +0200

    Fix CountMoviesAsync

--- Cut-Roll-Users/src/Cut-Roll-Users.Infrastructure/Common/DataProcessing/SqlDataReaderService.cs
+++ Cut-Roll-Users/src/Cut-Roll-Users.Infrastructure/Common/DataProcessing/SqlDataReaderService.cs
@@ -133,6 +133,6 @@ public class SqlDataReaderService : ISqlDataReaderService
 
     public async Task<int> GetTotalMovieCountAsync()
     {
-        return await _movieService.GetMoviesWithoutEmbeddingsCountAsync();
+        return await _movieService.CountMoviesAsync();
     }
 }
